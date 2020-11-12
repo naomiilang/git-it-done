0d82982dffbd26883be5864814307cee5b9219c0
Naomi <naomilang1212@gmail.com>
2020-11-12 11:39:26 -0800

    error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -50,26 +50,31 @@ var displayRepos = function(repos, searchTerm) {
     repoSearchTerm.textContent = searchTerm;
 
     // loop over repos
-    for (var i = 0; i < repos.length; i++) {
-        // format repo name
-        var repoName = repos[i].owner.login + "/" + repos[i].name;
-    
-        // create a container for each repo
-        var repoEl = document.createElement("div");
-        repoEl.classList = "list-item flex-row justify-space-between align-center";
-    
-        // create a span element to hold repository name
-        var titleEl = document.createElement("span");
-        titleEl.textContent = repoName;
-    
-        // append to container
-        repoEl.appendChild(titleEl);
-        
-        //create a status element
-        var statusEl = document.createElement("span");
-        statusEl.classList = "flex-row align-center";
+for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+  
+    // create a container for each repo
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html");
+    // create a link for each repo
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+    // create a span element to hold repository name
 
-        //check if current repo has issues or not
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+    
+    // append to container
+    repoEl.appendChild(titleEl);
+        
+    //create a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    //check if current repo has issues or not
         if (repos[i].open_issues_count > 0) {
             statusEl.innerHTML = 
             "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
--- assets/js/single.js
+++ assets/js/single.js
@@ -1,27 +1,29 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl).then(function(response){
-        if(response.ok){
-            response.json().then(function(data){
-               //pass response data to dom function
-               displayIssues(data);
-
-               //check if api has paginated issues
-               if (response.headers.get("Link")){
-                   displayWarning(repo);
-               }
-            });
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayIssues(data);
+  
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
         }
-        else {
-            alert("There was a problem with your request!");
-        }
-    });
+      });
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
+    }
+  });
+    
 };
-getRepoIssues("facebook/react");
 
 var displayIssues = function(issues) {
     
@@ -72,3 +74,18 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+};
+
+getRepoName();
